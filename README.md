2b94b7c17153f4e94d64e9d1482d0c71afa20b9c
Rishi Raj <rishiraj824@gmail.com>
2019-12-20 02:12:42 -0500

    Added 2 bit branch predictor

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # branch-predictor
-2 bit saturated branch predictor with BHR
+2 bit saturated branch predictor with BHR for NYU ECE GY 6193 Lab 2 Assignment
+
+## Steps to run
+
+`g++ predictor.cpp`
+
+`./a.out config.txt trace.txt`
+
+## Details 
+
+Simulate a correlating branch predictor that makes use of 2-bit saturating counters. A text file containing a trace of branch instructions consisting of the PC at which each branch occurs, and whether the branch is Taken or Not Taken. Write code to evaluate the performance of a correlating branch predictor on this trace. Your output file indicates, for each branch in the trace, whether it was predicted as Taken or Not Taken. 
+
+### Branch Predictor Architecture
+Your design must consist of 2^m+k 2-bit saturating counters indexed using m LSBs of each branch instruction and a k bit Branch History Register (BHR) that records the outcomes of the previous k branches. Each 2-bit saturating predictor starts in the 11 (Predict Taken with High Confidence) state and is updated as per the finite state machine discussed in Lecture 12. Further, you can assume that the BHR is initialized assuming the previous k branches were Taken.  
+The values of m and k are specified in a config file config.txt. 
+The maximum values of m = 20 and k = 8.
+
+### Config File
+The config file config.txt contains a single line with the value of m and k. A sample file for m=12 and k=2 is provided.
+
+### Trace File
+The trace file, trace.txt, contains one branch per line. Each line has the PC for the corresponding branch (in hex format) followed by a single bit indicating Taken (1) or Not Taken (0). A sample trace file is provided.  A sample trace file is provided.
+
+### Output Format
+The output from your simulator should be a file trace.out that has one line per branch. Each line has a single bit which indicates whether that branch was predicted as Taken (1) or Not Taken (0).
---
+++ predictor.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <bitset>
+#include <vector>
+#include <stdlib.h>     /* strtoull */
+
+//state:
+#define SN "00" // Strong Not Taken
+#define WN "01" // Weakly Not Taken
+#define WT "10" // Weakly Taken
+#define ST "11" // Strong Taken
+
+using namespace std;
+
+
+static inline long get_last_n_bits(long num, int nbbits) {
+   return num & ((1L<<nbbits)-1);
+}
+
+int main (int argc, char** argv) {
+	ifstream config;
+	config.open(argv[1]);
+	int m, k;
+	config >> m >> k;
+
+    int z = pow(2,m);
+    string pht[z][k];
+    string bhr;
+    // initialize a bhr with k length
+    for(int i=0;i<k;i++){
+        bhr += '1';
+    }
+    // initialize everything in pht to 11
+    for(int i=0;i<z;i++){
+        for(int j=0;j<k;j++){
+            pht[i][j] = ST;
+        }
+    }
+
+	config.close();
+
+	ofstream out;
+	string out_file_name = string(argv[2]) + ".out";
+	out.open(out_file_name.c_str());
+	
+	ifstream trace;
+	trace.open(argv[2]);
+    int counter =1;
+	while (!trace.eof()) {
+		unsigned long pc; bool taken;
+		trace >> std::hex >> pc >> taken;
+        int val = get_last_n_bits(pc, m); // last m bits in decimal
+        int bhrval = strtoull(bhr.c_str(),NULL,2);
+        
+        bool prediction;
+        
+        // updating bhr        
+        for(int i=k-1;i>0;i--){            
+            if(taken){
+                bhr[0] = '1'; 
+            }
+            else {
+                bhr[0] = '0';
+            } 
+            bhr[i]=bhr[i-1];
+        }
+        
+        bhrval = strtoull(bhr.c_str(),NULL,2);
+        
+        // updating pht
+        if(taken){
+            if(pht[val][bhrval]==ST||pht[val][bhrval]==WT){
+                pht[val][bhrval]=ST;
+                prediction=true;
+            }
+            else if(pht[val][bhrval]==WN){
+                pht[val][bhrval]=ST;
+                prediction=false;
+            }
+            else if(pht[val][bhrval]==SN){
+                pht[val][bhrval]=WN;
+                prediction=false;
+            }
+        }
+        else {
+            if(pht[val][bhrval]==WT){
+                pht[val][bhrval]=SN;
+                prediction=true;
+            }
+            else if(pht[val][bhrval]==SN||pht[val][bhrval]==WN){
+                pht[val][bhrval]=SN;
+                prediction=false;
+            }
+            else if(pht[val][bhrval]==ST){
+                pht[val][bhrval]=WT;
+                prediction=true;
+            }
+        }
+        counter++;
+		out << prediction << endl;
+	}
+	 
+	trace.close();	
+	out.close();
+}
